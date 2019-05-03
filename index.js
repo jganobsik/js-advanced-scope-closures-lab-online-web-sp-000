9d780cb1486bbd03b3fa0f6706bbb9981f4eaead
Joe Ganobsik <jganobsik@gmail.com>
2019-05-03 18:59:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,3 +20,13 @@ function produceTipCalculator(pct) {
     return fare * pct;
   }
 }
+
+function createDriver() {
+  let driverId = 0
+  return class {
+    constructor(name) {
+      this.id = driverId++;
+      this.name = name;
+    }
+  }
+}
